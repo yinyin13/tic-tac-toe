ba7cdf13bb303099b11a0a04c5b11f030447eb7a
MatildaHuang <148395165+yinyin13@users.noreply.github.com>
2023-11-01 23:06:06 +0000

    update logic.py and cli.py

--- cli.py
+++ cli.py
@@ -3,42 +3,60 @@
 # For core game logic, see logic.py.
 
 from logic import make_empty_board
+from logic import print_board
 from logic import place_input
 from logic import get_winner
 from logic import other_player
 
 board_index = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
+def get_player_input(player):
+  print(f"Player {player}'s turn!")
+  user_input = input("Please choose an empty spot by typing its corresponding number: ")
+
+  if int(user_input) in range(1, 10):
+    place_input(board, player, user_input)
+  
+  else:
+    print("Invalid input!")
+    get_player_input(player)
+
+
 if __name__ == '__main__':
-    board = make_empty_board()
-    winner = None
+  # Get an empty board
+  board = make_empty_board()
+  winner = None
+  player = 'X'  # Let's start with player X
 
-    print("This is our board:")
-    for row in board:
-      print(row)
-    print("Here's what number each spot is represented by.")
-    for r in board_index:
-      print(r)
+  # Provide instructions for input
+  print("This is our board:")
+  print_board(board)
 
-    while winner == None:
-      # Player X goes first
-        print("TODO: Player X's turn!")
-        player = 'X'
-        user_input = input("Please choose an empty spot by typing its corresponding number: ")
-        place_input(player, user_input)
+  print("Here's what number each spot is represented by:")
+  print_board(board_index)
 
-      # Update the board
-        for row in board:
-          print(row)
+  while winner is None:
+    # Player's turn
+    get_player_input(player)
 
-        # Player O goes next
-        print("TODO: Player O's turn!")
-        player = 'O'
-        user_input = input("Please choose an empty spot by typing its corresponding number: ")
-        place_input(player, user_input)
+    # Update the board
+    print_board(board)
 
-      # Update the board
-        for row in board:
-            print(row)
+    # Check if there's a winner
+    winner = get_winner(board)
 
-        winner = get_winner(board)
+    # If there's a winner or a draw, break the loop
+    if winner is not None:
+      break
+
+    # Switch the player for next turn
+    player = other_player(player)
+
+    # Check again for winner
+    winner = get_winner(board)
+
+  # Print winner
+  if winner == "Draw":
+    print(f"It's a {winner}")
+  else:
+    print(f"{winner} won")
--- logic.py
+++ logic.py
@@ -5,44 +5,58 @@ def make_empty_board():
         [None, None, None],
     ]
 
+def print_board(board):
+  for row in board:
+    print(row)
+
 def get_winner(board):
-    winner = None
-
-    # Check if any row has a winner
     for r in range(3):
-      if board[r][0] == board[r][1] == board[r][2] and board[r][0] != None:
-        winner = True
-
-    # Check if any column has a winner
-      elif board[0][r] == board[1][r] == board[2][r] and board[0][r] != None:
-        winner = True
+        # Check if any row has a winner
+        if board[r][0] is not None and (board[r][0] == board[r][1] == board[r][2]):
+            return board[r][0]
+            
+        # Check if any column has a winner
+        elif board[0][r] is not None and (board[0][r] == board[1][r] == board[2][r]):
+            return board[0][r]
 
     # Check if any diagonal has a winner
-      elif board[1][1] != None and board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
-        winner = True
+    if board[1][1] is not None and (board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]):
+        return board[1][1]
 
-    # If no one wins, it's a draw
-    if winner is False:
-      print('It's a draw')
+    # If no one wins and board is full, it's a draw
+    flat_board = [item for row in board for item in row]
+    if None not in flat_board:
+        return "Draw"
 
-    return winner
+    # If the board isn't full and no one has won yet
+    return None
 
-def place_input(player, user_input):
-  if int(user_input) == 1 and board[0][0] == None:
+
+def place_input(board, player, user_input):
+  if int(user_input) == 1 and board[0][0] is None:
     board[0][0] = player
-  elif int(user_input) == 2 and board[0][1] == None:
+  elif int(user_input) == 2 and board[0][1] is None:
     board[0][1] = player
-  elif int(user_input) == 3 and board[0][2] == None:
+  elif int(user_input) == 3 and board[0][2] is None:
     board[0][2] = player
-  elif int(user_input) == 4 and board[1][0] == None:
+  elif int(user_input) == 4 and board[1][0] is None:
     board[1][0] = player
-  elif int(user_input) == 5 and board[1][1] == None:
+  elif int(user_input) == 5 and board[1][1] is None:
     board[1][1] = player
-  elif int(user_input) == 6 and board[1][2] == None:
+  elif int(user_input) == 6 and board[1][2] is None:
     board[1][2] = player
-  elif int(user_input) == 7 and board[2][0] == None:
+  elif int(user_input) == 7 and board[2][0] is None:
     board[2][0] = player
-  elif int(user_input) == 8 and board[2][1] == None:
+  elif int(user_input) == 8 and board[2][1] is None:
     board[2][1] = player
-  elif int(user_input) == 9 and board[2][2] == None:
+  elif int(user_input) == 9 and board[2][2] is None:
     board[2][2] = player
+
+def other_player(player = ''):
+  if player == 'X':
+    player = 'O'
+  
+  else:
+    player = 'X'
+  
+  return player
